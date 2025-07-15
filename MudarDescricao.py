12ababb1cdcb8ce6d25bed49b76c522e335329d9
joaomalfatti <joao.cursos1210@gmail.com>
2025-07-15 19:56:49 -0400

    Ajustes necessários visual

--- MudarDescricao.py
+++ MudarDescricao.py
@@ -21,7 +21,7 @@ def alterar_descricao_local(nova_descricao):
         winreg.SetValueEx(chave_registro, "srvcomment", 0, winreg.REG_SZ, nova_descricao)
         winreg.CloseKey(chave_registro)
 
-        print(f"\n✅ Descrição alterada com sucesso para: {nova_descricao}")
+        print(f"\n Descrição alterada com sucesso para: {nova_descricao}")
         return True
 
     except PermissionError:
@@ -33,7 +33,7 @@ def alterar_descricao_local(nova_descricao):
 
 def reiniciar_servico_lanman():
     print("\n Reiniciando serviço 'lanmanserver'...")
-    resultado = os.system("net stop lanmanserver && net start lanmanserver")
+    resultado = os.system(" net stop lanmanserver && net start lanmanserver")
     if resultado == 0:
         print(" Serviço reiniciado com sucesso.")
     else:
@@ -43,15 +43,20 @@ def main():
     if not is_admin():
         print(" Este script precisa ser executado como administrador.")
         return
+    
+    os.system("cls")
+    print("\n" + "="*50)
+    print("=== Mudar Descrição do Computador - v1.1 ===")
+    print("="*50)
 
-    descricao = input(" Qual nome você deseja colocar como descrição do computador? ").strip()
+    descricao = input("\n Qual nome você deseja colocar como descrição do computador? ").strip()
 
     if not descricao:
         print(" A descrição não pode estar vazia.")
         return
 
     if alterar_descricao_local(descricao):
-        resposta = input("\nDeseja reiniciar o serviço 'lanmanserver' agora? (s/n): ").lower()
+        resposta = input("\n Deseja reiniciar o serviço 'lanmanserver' agora? (s/n): ").lower()
         if resposta == 's':
             reiniciar_servico_lanman()
         else:
@@ -61,5 +66,5 @@ if __name__ == "__main__":
     main()
 
 
-""" Version: 1.0.0
+""" Version: 1.1.0
 Creator: João Malfatti """
